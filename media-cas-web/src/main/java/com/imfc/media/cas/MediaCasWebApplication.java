a55e9e1911f4f264db13bf045bde3025f7363258
dipu <15373025232@163.com>
2019-07-15 19:25:51 +0800

    添加cas模块，修改工作站页面

---
+++ media-cas-web/src/main/java/com/imfc/media/cas/MediaCasWebApplication.java
@@ -0,0 +1,13 @@
+package com.imfc.media.cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaCasWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaCasWebApplication.class, args);
+    }
+
+}
